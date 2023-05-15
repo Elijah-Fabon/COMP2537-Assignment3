414d0b5853511e3373b4c0e88853194c5f6dd35c
Elijah_Fabon <efabon2@my.bcit.ca>
2023-05-15 16:45:30 -0700

    change initialization point of filteredPokemon

--- pokemon.js
+++ pokemon.js
@@ -12,18 +12,6 @@ const setup = async () => {
   console.log(response.data.results);
 
   pokemon = response.data.results;
-  filteredPokemon = pokemon.filter((monster) => {
-    if (selectedTypes.size === 0) return true;
-    let hasAllSelectedTypes = true;
-    for (let type of selectedTypes) {
-      if (!monster.types.includes(type)) {
-        hasAllSelectedTypes = false;
-        break;
-      }
-    }
-    return hasAllSelectedTypes;
-  });
-  console.log(filteredPokemon);
   numPages = Math.ceil(pokemon.length / numPerPage);
   console.log(numPages);
 
@@ -116,10 +104,22 @@ const setup = async () => {
       currentPage = numPages;
     }
 
+  filteredPokemon = pokemon.filter((monster) => {
+    if (selectedTypes.size === 0) return true;
+    let hasAllSelectedTypes = true;
+    for (let type of selectedTypes) {
+      if (!monster.types.includes(type)) {
+        hasAllSelectedTypes = false;
+        break;
+      }
+    }
+    return hasAllSelectedTypes;
+  });
+  console.log(filteredPokemon);
   console.log(currentPage);
   console.log((currentPage - 1) * numPerPage);
   console.log(((currentPage - 1) * numPerPage) + numPerPage);
-  console.log(pokemon.length);
+  console.log(filteredPokemon.length);
 
   $("#countText").empty();
   $("#countText").text(`Showing ${(currentPage - 1) * numPerPage + 1} to ${numPerPage * currentPage} of ${filteredPokemon.length} Pokémon`);
